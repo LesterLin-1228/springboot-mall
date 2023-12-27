3ce5579eae7142fdf77c7ac3f36479b802375940
LesterLin <newa5812763@gmail.com>
2023-12-27 23:11:34 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/lesterlin/springbootmall/dao/ProductDao.java
+++ src/main/java/com/lesterlin/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.lesterlin.springbootmall.dao;
 import com.lesterlin.springbootmall.dto.ProductRequest;
 import com.lesterlin.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
